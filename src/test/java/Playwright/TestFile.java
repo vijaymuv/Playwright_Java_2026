2a0fbf4b8d16816528df7a785077512d72adac1f
vijay <benvvm@gmail.com>
2026-04-28 12:27:24 +0530

    Added files

---
+++ src/test/java/Playwright/TestFile.java
@@ -0,0 +1,14 @@
+package Playwright;
+
+import Utilities.App_Constants;
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.assertions.PageAssertions;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.setDefaultAssertionTimeout;
+
+public class Assertions_{
+    public static void main(String[] args) throws InterruptedException {
+        Playwright playwright = Playwright.create();
+      
+    }
+}
